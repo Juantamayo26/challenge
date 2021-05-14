2f1b35081e50d77c2bc1671b75f8e14b5d5cedcd
Juan Tamayo <juancyepest@gmail.com>
2021-05-14 13:45:30 -0500

    Added Buyers API

--- README.md
+++ README.md
@@ -1,27 +1,38 @@
-#API REST
-##Golang
+# API REST
+
+## Golang
+
 endpoint score user
 endpoint all score of the users
 
-#INTERFACE
-##Vuejs
+# INTERFACE
+
+## Vuejs
+
 Name of the user
 Best scores
 
-#Create game
-##Phaser game engine
+# Create game
+
+## Phaser game engine
+
 Green apple +1
 Walls change every 10 apples
 Red apples -2 
 3D
 Follow pointer of the mouse 
 
-#Websockets (optional)
+# Websockets (optional)
 
 
-###Backend: Go
-###Frontend: Typescript
-###Database: CockroachDB
-###API Router: chi
-###Interface: Vue.js & Vuetify
-###GameEngine: Phaser
+### Backend: Go
+
+### Frontend: Typescript
+
+### Database: CockroachDB
+
+### API Router: chi
+
+### Interface: Vue.js & Vuetify
+
+### GameEngine: Phaser
---
+++ backend/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// Types
+type buyer struct {
+	ID   string `json:"ID"`
+	Name string `json:"Name"`
+	Age  int    `json:"Age"`
+}
+
+type allBuyers []buyer
+
+var buyers = allBuyers{
+	{
+		ID:   "12321",
+		Name: "Task one",
+		Age:  12,
+	},
+}
+
+func getBuyers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(buyers)
+}
+
+func getOneBuyer(w http.ResponseWriter, r *http.Request) {
+	buyerID := chi.URLParam(r, "ID")
+
+	fmt.Println(buyerID)
+	for _, b := range buyers {
+		if b.ID == buyerID {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(b)
+			return
+		}
+	}
+
+	fmt.Fprintf(w, "Invalid ID")
+}
+
+func createBuyer(w http.ResponseWriter, r *http.Request) {
+	var newBuyer buyer
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Error")
+	}
+
+	json.Unmarshal(reqBody, &newBuyer)
+
+	buyers = append(buyers, newBuyer)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newBuyer)
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/buyers", getBuyers)
+	r.Post("/buyers", createBuyer)
+	r.Get("/buyers/{ID}", getOneBuyer)
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
